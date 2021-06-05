db325010143165f8fc4a08cd55ddcce7d7c20534
Jarek <jareksedy@icloud.com>
2021-06-05 10:47:03 +0600

    добавил таббар

--- vkgb/LoginFormController.swift
+++ vkgb/LoginFormController.swift
@@ -10,5 +10,8 @@ import UIKit
 class LoginFormController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     
+    @IBAction func loginButton(_ sender: UIButton) {
+        // go to next view
+    }
     
 }
