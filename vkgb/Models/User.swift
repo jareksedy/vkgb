3a724849bade8a77e8ab26a08f9f22f1b213610f
Jarek <jareksedy@icloud.com>
2021-06-06 23:01:28 +0600

    New Dostoyevsky profile pic.

--- vkgb/Models/User.swift
+++ vkgb/Models/User.swift
@@ -11,6 +11,12 @@ struct User {
     let name: String
     let lastname: String
     let imageName: String?
+    
+    init(name: String, lastname:String, imageName: String? = nil) {
+        self.name = name
+        self.lastname = lastname
+        self.imageName = imageName ?? "defaultAvatar"
+    }
 }
 
 struct UserDataStorage {
@@ -19,10 +25,13 @@ struct UserDataStorage {
         User(name: "Фридрих", lastname: "Ницше", imageName: "Nietzsche"),
         User(name: "Артур", lastname: "Шопенгауэр", imageName: "Schopenhauer"),
         User(name: "Фёдор", lastname: "Достоевский", imageName: "Dostoyevsky"),
+        User(name: "Карл", lastname: "Юнг", imageName: "Jung"),
         User(name: "Иммануил", lastname: "Кант", imageName: "Kant"),
         User(name: "Альбер", lastname: "Камю", imageName: "Camus"),
+        User(name: "Людвиг", lastname: "Витгенштейн"),
         User(name: "Сёрен", lastname: "Кьеркегор", imageName: "Kierkegaard"),
         User(name: "Зигмунд", lastname: "Фрейд", imageName: "Freud"),
-        User(name: "Жан-Поль", lastname: "Сартр", imageName: "Sartre")
+        User(name: "Жан-Поль", lastname: "Сартр", imageName: "Sartre"),
+        User(name: "Карл", lastname: "Ясперс"),
     ]
 }
