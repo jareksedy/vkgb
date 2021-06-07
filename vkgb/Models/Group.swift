63e255cd72bb784d7b6aa28aaf1e91f81ed041bb
Jarek <jareksedy@icloud.com>
2021-06-07 22:42:16 +0600

    added group id and mygroups array.

--- vkgb/Models/Group.swift
+++ vkgb/Models/Group.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 struct Group {
+    let id: Int
     let name: String
     let description: String?
     let imageName: String?
     
-    init(name: String, description:String? = nil, imageName: String? = nil) {
+    init(id: Int, name: String, description:String? = nil, imageName: String? = nil) {
+        self.id = id
         self.name = name
         self.description = description
         self.imageName = imageName ?? "defaultGroupImage"
@@ -21,13 +23,16 @@ struct Group {
 
 struct GroupDataStorage {
     static var groups: [Group] = [
-        Group(name: "Экзистенциальная философия", description: "Интеллектуальный паблик, посвященный философии.", imageName: "existential"),
-        Group(name: "Другая философия", description: "Ну такое вот.", imageName: "other"),
-        Group(name: "Голожопые философы", description: "Открытая группа.", imageName: "naked"),
-        Group(name: "Афоризмы великих людей", description: "Какая-то херня с интернета.", imageName: "quote"),
-        Group(name: "Чоткие философы", description: "Здоровый образ жизни.", imageName: "chotk"),
-        Group(name: "Двумерные девочки и немецкие философы", description: "Культур-мультур.", imageName: "2d"),
-        Group(name: "Философия Брюса Ли", description: "Он такой херни не говорил.", imageName: "lee"),
-        Group(name: "Философия для тупых", description: "Постбыдло.", imageName: "tupoi"),
+        Group(id: 0, name: "Экзистенциальная философия", description: "Интеллектуальный паблик, посвященный философии.", imageName: "existential"),
+        Group(id: 1, name: "Другая философия", description: "Ну такое вот.", imageName: "other"),
+        Group(id: 2, name: "Голожопые философы", description: "Открытая группа.", imageName: "naked"),
+        Group(id: 3, name: "Афоризмы великих людей", description: "Какая-то херня с интернета.", imageName: "quote"),
+        Group(id: 4, name: "Чоткие философы", description: "Здоровый образ жизни.", imageName: "chotk"),
+        Group(id: 5, name: "Двумерные девочки и немецкие философы", description: "Культур-мультур.", imageName: "2d"),
+        Group(id: 6, name: "Философия Брюса Ли", description: "Он такой херни не говорил.", imageName: "lee"),
+        Group(id: 7, name: "Философия для тупых", description: "Постбыдло.", imageName: "tupoi"),
     ]
+    
+    static var myGroups: [Group] = groups.filter{$0.id == 3 || $0.id == 5 || $0.id == 7}
+    
 }
