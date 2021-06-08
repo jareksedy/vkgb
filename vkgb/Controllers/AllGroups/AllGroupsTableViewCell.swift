20e025d1fc5f3da32c3d6f977a0efc6be399228b
Jarek <jareksedy@icloud.com>
2021-06-08 21:06:45 +0600

    added outlet and action for add group button.

--- vkgb/Controllers/AllGroups/AllGroupsTableViewCell.swift
+++ vkgb/Controllers/AllGroups/AllGroupsTableViewCell.swift
@@ -11,12 +11,17 @@ class AllGroupsTableViewCell: UITableViewCell {
     @IBOutlet weak var AllGroupsImage: UIImageView!
     @IBOutlet weak var AllGroupsName: UILabel!
     @IBOutlet weak var AllGroupsDescription: UILabel!
+    @IBOutlet weak var addGroupButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func addGroupAction(_ sender: Any) {
+        print("hello")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
