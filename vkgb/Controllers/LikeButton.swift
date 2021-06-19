1a72b57681ada3aef8dd2fea1fc0b71b987ed0e0
Jarek <jareksedy@icloud.com>
2021-06-19 13:19:55 +0600

    modified likebutton

--- vkgb/Controllers/LikeButton.swift
+++ vkgb/Controllers/LikeButton.swift
@@ -37,41 +37,17 @@ class LikeButton: UIButton {
     // MARK: - Internal methods
     
     fileprivate func configure() {
+
+        self.setTitle("\(liked ? "♥" : "♡") \(counter)", for: .normal)
         
-        /*
-        let buttonWidth: CGFloat = 80
-        let buttonHeight: CGFloat = 40
-        self.frame = CGRect(x: ((superview!.bounds.maxX - buttonWidth) / 2) + buttonWidth,
-                            y: ((superview!.bounds.maxY - superview!.bounds.minY) / 2),
-                            width: buttonWidth,
-                            height: buttonHeight)
-         */
-        
-        self.setTitle("💟 Нра! (\(counter))", for: .normal)
-        
-        //self.titleEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
-        
-        self.backgroundColor = liked ? UIColor.systemPink : UIColor.systemBlue
-        self.setTitleColor(UIColor.white, for: .normal)
+        self.backgroundColor = liked ? UIColor.systemPink : UIColor.clear
+        self.setTitleColor(liked ? UIColor.white : UIColor.gray, for: .normal)
         self.clipsToBounds = true
         self.layer.cornerRadius = 4.0
-       //self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        
-        //let heartImageView = UIImageView()
-        //heartImageView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        //heartImageView.image = UIImage(systemName: "heart.circle.fill")
-        //heartImageView.tintColor = UIColor.white
-        //self.addSubview(heartImageView)
         
         addTarget(self,
                          action: #selector(onTap(_:)),
                          for: .touchUpInside)
-
-        
-        //self.setImage(UIImage(systemName: "heart.circle.fill"), for: .normal)
-        //self.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 0)
-        //self.titleEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 0)
         
     }
     
