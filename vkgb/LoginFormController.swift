18db4a6f05b3cdc112e9b43418275bcda4260ffb
Jarek <jareksedy@icloud.com>
2021-06-20 10:30:37 +0600

    added login button animation.

--- vkgb/LoginFormController.swift
+++ vkgb/LoginFormController.swift
@@ -11,7 +11,15 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func loginButton(_ sender: UIButton) {
-        // go to next view
+        UIButton.animate(withDuration: 0.75,
+                                    delay: 0,
+                                    usingSpringWithDamping: 0.25,
+                                    initialSpringVelocity: 0.75,
+                                    options: [.allowUserInteraction],
+                                    animations: {
+                                        self.loginButton.bounds = self.loginButton.bounds.insetBy(dx: -10, dy: -10)
+                                    },
+                                    completion: nil)
     }
     
 }
