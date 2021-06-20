9140e92bedaf9f914c3980e0445b7aaef7298b34
Jarek <jareksedy@icloud.com>
2021-06-20 11:45:16 +0600

    added animation screen with 3 dots.

---
+++ vkgb/Controllers/AnimationScreenViewController.swift
@@ -0,0 +1,133 @@
+//
+//  AnimationScreenViewController.swift
+//  vkgb
+//
+//  Created by Ярослав on 20.06.2021.
+//
+
+import UIKit
+
+class AnimationScreenViewController: UIViewController {
+    
+    func delay(_ delay:Double, closure:@escaping ()->()) {
+        let when = DispatchTime.now() + delay
+        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+    }
+
+    @IBOutlet weak var dotView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let dot1 = UIView()
+        let dot2 = UIView()
+        let dot3 = UIView()
+        
+        let dSize = 60
+        let sSize = 10
+        
+        dot1.frame = CGRect(x: 0, y: sSize, width: dSize, height: dSize)
+        dot2.frame = CGRect(x: dSize + sSize, y: sSize, width: dSize, height: dSize)
+        dot3.frame = CGRect(x: dSize * 2 + sSize * 2, y: sSize, width: dSize, height: dSize)
+        
+        dot1.backgroundColor = UIColor.systemPink
+        dot2.backgroundColor = UIColor.systemPink
+        dot3.backgroundColor = UIColor.systemPink
+        
+        dot1.layer.cornerRadius = CGFloat(dSize / 2)
+        dot2.layer.cornerRadius = CGFloat(dSize / 2)
+        dot3.layer.cornerRadius = CGFloat(dSize / 2)
+        
+        dot1.alpha = 0.5
+        dot2.alpha = 0.5
+        dot3.alpha = 0.5
+        
+        dot1.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        dot2.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        dot3.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        
+        dotView.addSubview(dot1)
+        dotView.addSubview(dot2)
+        dotView.addSubview(dot3)
+        
+        let initialDelay = 0.25
+        let animationDuration = 0.25
+        
+        delay(initialDelay){
+            
+            UIView.animate(withDuration: animationDuration,
+                           delay: 0,
+                           options: [],
+                           animations: {
+                            dot1.alpha += 0.5
+                            dot1.transform = CGAffineTransform(scaleX: 1, y: 1)
+                           },
+                           completion: {_ in
+                            UIView.animate(withDuration: animationDuration,
+                                           delay: 0,
+                                           options: [],
+                                           animations: {
+                                            dot2.alpha += 0.5
+                                            dot2.transform = CGAffineTransform(scaleX: 1, y: 1)
+                                           },
+                                           completion: {_ in
+                                            UIView.animate(withDuration: animationDuration,
+                                                           delay: 0,
+                                                           options: [],
+                                                           animations: {
+                                                            dot3.alpha += 0.5
+                                                            dot3.transform = CGAffineTransform(scaleX: 1, y: 1)
+                                                           },
+                                                           completion: {_ in
+                                                            UIView.animate(withDuration: animationDuration,
+                                                                           delay: 0,
+                                                                           options: [],
+                                                                           animations: {
+                                                                            dot1.alpha -= 0.5
+                                                                            dot1.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                                                                           },
+                                                                           completion: {_ in
+                                                                            UIView.animate(withDuration: animationDuration,
+                                                                                           delay: 0,
+                                                                                           options: [],
+                                                                                           animations: {
+                                                                                            dot2.alpha -= 0.5
+                                                                                            dot2.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                                                                                           },
+                                                                                           completion: {_ in
+                                                                                            UIView.animate(withDuration: animationDuration,
+                                                                                                           delay: 0,
+                                                                                                           options: [],
+                                                                                                           animations: {
+                                                                                                            dot3.alpha -= 0.5
+                                                                                                            dot3.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                                                                                                           },
+                                                                                                           completion: {_ in
+                                                                                                            
+                                                                                                            self.performSegue(withIdentifier: "goForth", sender: self)
+                                                                                                           })
+                                                                                           })
+                                                                           })
+                                                           })
+                                           })
+                           })
+            
+        }
+
+    }
+    
+
+    func setupView() {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
