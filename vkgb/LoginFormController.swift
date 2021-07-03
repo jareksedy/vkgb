f8532c810698a3d1d74d943a96d5525a80773dbd
Jarek <jareksedy@icloud.com>
2021-07-03 21:42:27 +0600

    fixed scrolling.

--- vkgb/LoginFormController.swift
+++ vkgb/LoginFormController.swift
@@ -11,12 +11,31 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     let login = "cthulhu"
     let password = "fhtagn!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -34,27 +53,52 @@ class LoginFormController: UIViewController {
                                                     preferredStyle: .alert)
             
             authFailedAlert.addAction(UIAlertAction(title: "Да и хрен с ним, впусти!",
-                                                 style: .default,
-                                                 handler: {_ in
-                                                    self.performSegue(withIdentifier: "authSegue", sender: self)
-                                                 }))
+                                                    style: .default,
+                                                    handler: {_ in
+                                                        self.performSegue(withIdentifier: "authSegue", sender: self)
+                                                    }))
             
             self.present(authFailedAlert, animated: true, completion: nil)
         }
     }
     
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    
+    
     @IBAction func loginButton(_ sender: UIButton) {
         UIButton.animate(withDuration: 0.75,
-                                    delay: 0,
-                                    usingSpringWithDamping: 0.25,
-                                    initialSpringVelocity: 0.75,
-                                    options: [.allowUserInteraction],
-                                    animations: {
-                                        self.loginButton.bounds = self.loginButton.bounds.insetBy(dx: -10, dy: -10)
-                                    },
-                                    completion: {_ in
-                                        self.auth()
-                                    })
+                         delay: 0,
+                         usingSpringWithDamping: 0.25,
+                         initialSpringVelocity: 0.75,
+                         options: [.allowUserInteraction],
+                         animations: {
+                            self.loginButton.bounds = self.loginButton.bounds.insetBy(dx: -10, dy: -10)
+                         },
+                         completion: {_ in
+                            self.auth()
+                         })
     }
     
 }
